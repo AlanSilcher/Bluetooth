fd207a03fccd09a407401a42fee3d617da5b52db
AlanSilcher <silcheralan@gmail.com>
2023-06-02 15:23:28 -0300

    Dale

--- src/Pages/Bluetooth/Bluetooth.jsx
+++ src/Pages/Bluetooth/Bluetooth.jsx
@@ -1,6 +1,6 @@
-import { useEffect } from "react";
+/* import { useEffect } from "react"; */
 import { useSelector, useDispatch } from "react-redux";
-import { requestBluetoothDevice, connectBluetoothDevice, writeBluetoothCharacteristic } from "../../redux/actions";
+import { requestBluetoothDevice/* , connectBluetoothDevice, writeBluetoothCharacteristic */ } from "../../redux/actions";
 
 export default function Bluetooth() {
   const dispatch = useDispatch();
