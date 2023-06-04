ac398b43979a2a6e7fcbaeda2f1b7823115c2f7f
AlanSilcher <silcheralan@gmail.com>
2023-06-04 02:06:36 -0300

    4

--- src/Pages/Bluetooth/Bluetooth.jsx
+++ src/Pages/Bluetooth/Bluetooth.jsx
@@ -1,40 +1,15 @@
-/* import { useEffect } from "react"; */
-import { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { requestBluetoothDevice, writeBluetoothCharacteristic, readBluetoothCharacteristic } from "../../redux/actions";
+import { requestBluetoothDevice } from "../../redux/actions";
 
 export default function Bluetooth() {
   const dispatch = useDispatch();
-  const bluetoothDevice = useSelector((state) => state.bluetoothDevice);
   const bluetoothConnected = useSelector((state) => state.bluetoothConnected);
-  const bluetoothRead = useSelector((state) => state.bluetoothRead);
-
-  console.log("esto es connect", bluetoothConnected)
-  console.log("esto es device", bluetoothDevice)
-  console.log("esto es read", bluetoothRead)
-
-
-  const [inputValue, setInputValue] = useState("");
 
   const handleRequestDevice = () => {
     dispatch(requestBluetoothDevice());
   };
 
-  const handleWriteCharacteristic = () => {
-    if (bluetoothConnected) {
-      dispatch(writeBluetoothCharacteristic(bluetoothDevice.characteristic, inputValue));
-    }
-  };
-
-  const handleReadCharacteristic = () => {
-    if (bluetoothConnected) {
-      dispatch(readBluetoothCharacteristic(bluetoothDevice.characteristic));
-    }
-  };
-
-  const handleInputChange = (e) => {
-    setInputValue(e.target.value);
-  };
+  console.log(bluetoothConnected)
 
   return (
     <div>
@@ -47,11 +22,6 @@ export default function Bluetooth() {
       {bluetoothConnected && (
         <div>
           <h1>¡Estás conectado!</h1>
-          <input type="text" value={inputValue} onChange={handleInputChange} />
-          <button onClick={handleWriteCharacteristic}>Enviar</button>
-          <button onClick={handleReadCharacteristic}>Recibir</button>
-          <p>Dato enviado: {inputValue}</p>
-          <p>Dato recibido: {bluetoothRead}</p>
         </div>
       )}
     </div>
